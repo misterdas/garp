8b9d91e5d40f0db21caad50b049fbd411c72763c
misterdas <gopaldas958@gmail.com>
2025-07-25 08:13:16 +0530

    Update garp_screener.py

--- garp_screener.py
+++ garp_screener.py
@@ -90,7 +90,7 @@ def fetchScreenerData(link):
     print(f"Total rows fetched: {len(data)}")
     return data
 
-pd.set_option("display.max_rows", None, "display.max_columns", None)
+pd.set_option("display.max_rows", None, "display.max_columns", None) # type: ignore
 
 # Fetch 3 month return
 print("=== Fetching 3 Month Returns ===")
